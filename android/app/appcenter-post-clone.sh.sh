ae724a9a74e0d5441b6ec9dce9ee8070946113e2
fzalewski <filipzalewski97@gmail.com>
2020-07-28 14:58:51 +0200

    init

--- android/app/appcenter-post-clone.sh.sh
+++ android/app/appcenter-post-clone.sh.sh
@@ -17,18 +17,14 @@ flutter doctor
 
 echo "Installed flutter to `pwd`/flutter"
 
-# make gradlew an executable
-chmod a+rx android/gradlew
-
-# if you need build an APK in addition to your AppBundle, uncomment lines below and last line of this script.
 # build APK
-flutter build apk --release --build-number=`date +%s`
+flutter build apk --release
+
+# if you need build bundle (AAB) in addition to your APK, uncomment line below and last line of this script.
+#flutter build appbundle
 
 # copy the APK where AppCenter will find it
 mkdir -p android/app/build/outputs/apk/; mv build/app/outputs/apk/release/app-release.apk $_
 
-# if you need build bundle (AAB) in addition to your APK, uncomment line below and last line of this script.
-flutter build appbundle --release --build-number=`date +%s`
-
 # copy the AAB where AppCenter will find it
-mkdir -p android/app/build/outputs/bundle/; mv build/app/outputs/bundle/release/app-release.aab $_
+#mkdir -p android/app/build/outputs/bundle/; mv build/app/outputs/bundle/release/app.aab $_
